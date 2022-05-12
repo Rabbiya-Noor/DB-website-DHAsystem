1b55700034a36bf029d17a28d4396bfd3d6db931
Rabbiya-Noor <101973592+Rabbiya-Noor@users.noreply.github.com>
2022-05-12 21:07:43 +0500

    Add files via upload

---
+++ travello/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import *
+	# Register your models here.
+
+admin.site.register(Adha)
+admin.site.register(Clients) 
+admin.site.register(Director)
+admin.site.register(Employees)
+admin.site.register(Plots)
